54acc0764b69883506df3747756cefd540b8d10d
Joe Martella <martellaj@live.com>
2016-10-09 16:01:42 -0700

    Sorts by date added.

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -6,20 +6,30 @@ export default class Results extends Component {
     constructor() {
         super();
 
+        this.state = {
+            sortOrder: "newest"
+        };
+
         this.renderResult = this.renderResult.bind(this);
+        this.transformResultsObjectToArray = this.transformResultsObjectToArray.bind(this);
+        this.onSortOrderChange = this.onSortOrderChange.bind(this);
     }
 
     /**
      * Custom methods
      */
 
-    renderResult(key) {
-        let gif = this.props.results[key];
+    onSortOrderChange(e) {
+        this.setState({ sortOrder: e.target.value });
+    }
+
+    renderResult(result) {
+        let key = result.timestamp;
 
         return (
             <Result
                 key={key}
-                result={gif}
+                result={result}
                 gifId={key}
                 selectGif={this.props.selectGif}
                 isSelected={key === this.props.selectedGif}
@@ -28,14 +38,44 @@ export default class Results extends Component {
         );
     }
 
+    sort(results, order) {
+        switch (this.state.sortOrder) {
+            case "newest": // Newest to oldest
+                return results.sort(this._newestToOldestSort);
+            case "oldest": // Oldest to newest
+                return results.sort(this._oldestToNewestSort);
+            case "most relevant": // Most relevant
+                break;
+            default: // Newest to oldest
+                return results.sort(this._newestToOldestSort);
+        }
+    }
+
+    transformResultsObjectToArray() {
+        let results = [];
+
+        Object.keys(this.props.results).map((key) => {
+            let result = this.props.results[key];
+            result.timestamp = key;
+            results.push(result);
+
+            return null;
+        });
+
+        return results;
+    }
+
     renderResults() {
         let hasResults = Object.keys(this.props.results).length > 0;
         let results;
 
         if (hasResults) {
+            let resultsArray = this.transformResultsObjectToArray();
+            resultsArray = this.sort(resultsArray, this.state.sortOrder);
+
             results = (
                 <div className="results-container">
-                    {Object.keys(this.props.results).map(this.renderResult)}
+                    {resultsArray.map(this.renderResult)}
                 </div>
             );
         } else {
@@ -43,6 +83,14 @@ export default class Results extends Component {
         }
 
         return results;
+    }
+
+    _newestToOldestSort(a, b) {
+        return b.timestamp.slice(4) - a.timestamp.slice(4);
+    }
+
+    _oldestToNewestSort(a, b) {
+        return a.timestamp.slice(4) - b.timestamp.slice(4);
     }
 
     /**
@@ -53,6 +101,10 @@ export default class Results extends Component {
         return (
             <div className="results-component">
                 <h2>results</h2>
+                <select value={this.state.sortOrder} onChange={(e) => this.onSortOrderChange(e)}>
+                    <option value="newest">Newest to oldest</option>
+                    <option value="oldest">Oldest to newest</option>
+                </select>
                 {this.renderResults()}
             </div>
         );
