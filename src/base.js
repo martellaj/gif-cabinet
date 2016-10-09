7dcc451c2694dbb3f5bc0c41e56a251aebaea49f
Joe Martella <martellaj@live.com>
2016-10-08 19:45:12 -0700

    Adds Firebase to application.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+let base = Rebase.createClass({
+    apiKey: "AIzaSyCJNyUPxrtWmwV_cIQoSeK4klp4yVKmwN4",
+    authDomain: "gif-cabinet.firebaseapp.com",
+    databaseURL: "https://gif-cabinet.firebaseio.com"
+});
+
+export default base;
