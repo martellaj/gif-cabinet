2b40f496dd7e92fccfb2871b2a53ae5f9e28d531
Joe Martella <martellaj@live.com>
2016-10-09 18:34:10 -0700

    Gets some search relevance code working.

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -12,10 +12,21 @@ export default class Results extends Component {
 
         this.renderResult = this.renderResult.bind(this);
         this.onSortOrderChange = this.onSortOrderChange.bind(this);
+        this.processRelevance = this.processRelevance.bind(this);
     }
 
     onSortOrderChange(e) {
         this.setState({ sortOrder: e.target.value });
+    }
+
+    processRelevance(results) {
+        return results.map(result => {
+            result.relevance = result.tags.filter(tag => {
+                return this.props.query.includes(tag);
+            }).length;
+
+            return result;
+        });
     }
 
     sort(results, order) {
@@ -25,7 +36,15 @@ export default class Results extends Component {
             case "oldest": // Oldest to newest
                 return results.sort(this._oldestToNewestSort);
             case "most relevant": // Most relevant
-                break;
+                if (this.props.query.length > 0) {
+                    results = this.processRelevance(results);
+                    results = results.filter(result => {
+                        return result.relevance > 0;
+                    });
+                    return results.sort(this._mostRelevantSort);
+                } else {
+                    return results.sort(this._newestToOldestSort);
+                }
             default: // Newest to oldest
                 return results.sort(this._newestToOldestSort);
         }
@@ -65,12 +84,31 @@ export default class Results extends Component {
         return resultsMarkup;
     }
 
+    _mostRelevantSort(a, b) {
+        return b.relevance - a.relevance;
+    }
+
     _newestToOldestSort(a, b) {
         return b.timestamp.slice(4) - a.timestamp.slice(4);
     }
 
     _oldestToNewestSort(a, b) {
         return a.timestamp.slice(4) - b.timestamp.slice(4);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        // TODO: Fix this.
+        let hasQuery = !!nextProps.query[0];
+
+        if (hasQuery) {
+            this.setState({
+                sortOrder: "most relevant"
+            });
+        } else {
+            this.setState({
+                sortOrder: "newest"
+            });
+        }
     }
 
     /**
@@ -84,6 +122,7 @@ export default class Results extends Component {
                 <select className="sort-order" value={this.state.sortOrder} onChange={(e) => this.onSortOrderChange(e)}>
                     <option value="newest">Newest to oldest</option>
                     <option value="oldest">Oldest to newest</option>
+                    <option value="most relevant">Most relevant</option>
                 </select>
                 {this.renderResults()}
             </div>
@@ -94,5 +133,7 @@ export default class Results extends Component {
 Results.propTypes = {
     results: React.PropTypes.array.isRequired,
     selectGif: React.PropTypes.func.isRequired,
-    selectedGif: React.PropTypes.string.isRequired
+    selectedGif: React.PropTypes.string.isRequired,
+    unselectGif: React.PropTypes.func.isRequired,
+    query: React.PropTypes.array.isRequired
 };
