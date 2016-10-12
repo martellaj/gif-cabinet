08d32f43bad87e5b7bceeed7788281b44149ea38
Joe Martella <martellaj@live.com>
2016-10-11 22:06:08 -0700

    MASSIVE overhaul to Results component.
    - Basically a rewrite after learning that render function gets called after each state change.

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -7,105 +7,43 @@ export default class Results extends Component {
         super();
 
         this.state = {
-            sortOrder: "newest",
-            isQueryActive: false,
-            isInitialLoad: true,
-            resultsSize: 0
+            sortOrder: 'newest'
         };
 
         this.renderResult = this.renderResult.bind(this);
         this.onSortOrderChange = this.onSortOrderChange.bind(this);
-        this.processRelevance = this.processRelevance.bind(this);
     }
 
     onSortOrderChange(e) {
-        let newSortOrder = e.target.value;
-
-        // Sort results and update results.
-        let results = this.props.results;
-        results = this.sort(results, newSortOrder);
-        this.props.updateResults(results);
-
         // Update sortOrder state.
         this.setState({
-            sortOrder: newSortOrder
+            sortOrder: e.target.value
         });
     }
 
-    processRelevance(results) {
+    /**
+     * Helper function that adds a relevancy score to each result
+     * based on the current query. Right now, it's not very sophisticated.
+     * One point per matched tag.
+     */
+    scoreRelevance(results, query) {
         return results.map(result => {
             result.relevance = result.tags.filter(tag => {
-                return this.props.query.includes(tag);
+                return query.includes(tag);
             }).length;
 
             return result;
-        });
+        })
     }
 
-    sort(results, order) {
-        // Filter out GIFs that don't match query.
-        if (this.state.isQueryActive) {
-            results = this.processRelevance(results);
-            results = results.filter(result => {
-                return result.relevance > 0;
-            });
-        }
-
-        switch (order) {
-            case "newest": // Newest to oldest
-                return results.sort(this._newestToOldestSort);
-            case "oldest": // Oldest to newest
-                return results.sort(this._oldestToNewestSort);
-            case "most relevant": // Most relevant
-                return results.sort(this._mostRelevantSort);
-            default: // Newest to oldest
-                return results.sort(this._newestToOldestSort);
-        }
-    }
-
-    renderResult(result) {
-        return (
-            <Result
-                key={result.key}
-                result={result}
-                timestamp={result.timestamp}
-                selectGif={this.props.selectGif}
-                isSelected={result.timestamp === this.props.selectedGif}
-                unselectGif={this.props.unselectGif}
-            />
-        );
-    }
-
-    renderResults() {
-        let results = this.props.results;
-        let resultsMarkup = <p>no results</p>;
-
-        if (results.length > 0) {
-            resultsMarkup = (
-                <div className="results-container">
-                    {results.map(this.renderResult)}
-                </div>
-            );
-        }
-
-        return resultsMarkup;
-    }
-
-    _mostRelevantSort(a, b) {
-        return b.relevance - a.relevance;
-    }
-
-    _newestToOldestSort(a, b) {
-        return b.timestamp - a.timestamp;
-    }
-
-    _oldestToNewestSort(a, b) {
-        return a.timestamp - b.timestamp;
-    }
-
-    isQueryActive(query) {
-        for (let i = 0; i < query.length; i++) {
-            if (query[i]) {
+    /**
+     * Helper function to ensure that there are valid tags in the
+     * query. If we remove tags from queries, they may be "undefined"
+     * while state is changing.
+     */
+    queryIsValid(query) {
+        for (let tag of query) {
+            if (!!tag) {
                 return true;
             }
         }
@@ -113,37 +51,86 @@ export default class Results extends Component {
         return false;
     }
 
-    componentWillReceiveProps(nextProps) {
-        // TODO: Add condition if collection size is different.
-        if ((this.state.isInitialLoad && nextProps.isDoneLoadingGifs && nextProps.results.length > 0) ||
-             this.state.resultsSize !== nextProps.results.length) {
-            // Sort results and update results.
-            let results = nextProps.results;
-            results = this.sort(results, this.state.sortOrder);
-            this.props.updateResults(results);
-
-            // Update state.
-            this.setState({
-                isInitialLoad: false,
-                resultsSize: results.length
+    /**
+     * Helper function that does the heavy lifting of pruning and
+     * sorting results based on query and sort order.
+     */
+    pruneAndSort(results, sortOrder, query) {
+        // Prune GIFs that don't match query (if we have one).
+        if (this.queryIsValid(query)) {
+            results = this.scoreRelevance(results, query);
+            results = results.filter(result => {
+                return result.relevance > 0;
             });
         }
 
-        let isQueryActive = this.isQueryActive(nextProps.query);
-        this.setState({
-            isQueryActive
-        });
-
-        if (isQueryActive) {
-            this.setState({
-                sortOrder: "most relevant"
-            });
+        // Sort results based on sort order.
+        switch (sortOrder) {
+            case 'newest': // Newest to oldest
+                return results.sort(function(a, b) {
+                    return b.timestamp - a.timestamp;
+                });
+            case 'oldest': // Oldest to newest
+                return results.sort(function(a, b) {
+                    return a.timestamp - b.timestamp;
+                });
+            case 'most relevant': // Most relevant
+                return results.sort(function(a, b) {
+                    return b.relevance - a.relevance;
+                });
+            default: // Newest to oldest
+                return results.sort(function(a, b) {
+                    return b.timestamp - a.timestamp;
+                });
         }
     }
 
     /**
-     * Render function
+     * Takes the subset of GIFs (results), and prunes out GIFs
+     * that have no relevance based on query (if there is one),
+     * then sorts them based on the current sort order.
      */
+    processResults() {
+        let results = this.props.results;
+        let sortOrder = this.state.sortOrder;
+        let query = this.props.query;
+
+        return this.pruneAndSort(results, sortOrder, query);
+    }
+
+    renderResult(result) {
+        return (
+            <Result
+                key={result.key}
+                isSelected={result.timestamp === this.props.selectedGif}
+                result={result}
+                selectGif={this.props.selectGif}
+                timestamp={result.timestamp}
+                unselectGif={this.props.unselectGif}
+            />
+        );
+    }
+
+    renderResults() {
+        let resultsMarkup;
+
+        // Process results based on query and sort order.
+        let results = this.processResults();
+
+        // Returns results markup if we have results, or "no results"
+        // markup if we don't.
+        if (results.length > 0) {
+            resultsMarkup = (
+                <div className="results-container">
+                    {results.map(this.renderResult)}
+                </div>
+            );
+        } else {
+            resultsMarkup = <p>no results</p>;
+        }
+
+        return resultsMarkup;
+    }
 
     render() {
         return (
@@ -161,9 +148,9 @@ export default class Results extends Component {
 }
 
 Results.propTypes = {
+    query: React.PropTypes.array.isRequired,
     results: React.PropTypes.array.isRequired,
-    selectGif: React.PropTypes.func.isRequired,
     selectedGif: React.PropTypes.number.isRequired,
-    unselectGif: React.PropTypes.func.isRequired,
-    query: React.PropTypes.array.isRequired
+    selectGif: React.PropTypes.func.isRequired,
+    unselectGif: React.PropTypes.func.isRequired
 };
